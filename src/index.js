8ac50558c8cd1b8a7cf4bc0e12aff87e1addddcf
Murali Krishna M <74974358+M-MuraliKrishna@users.noreply.github.com>
2023-01-03 23:11:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 var http = require("http");
 
+const PORT = 8081;
+
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+
+res.writeHead(200);
+
+switch(req.url){
+
+case "/welcome":
+
+res.end("Welcome to Dominos !");
+
+break;
+
+case "/contact":
+
+res.end({
+
+phone: '18602100000',
+
+email: 'guestcaredominos@jublfood.com'
+
+});
+
+break;
+
+default:
+
+res.writeHead(404);
+
 }
+
+}
+
+httpServer.listen(PORT);
 
 module.exports = httpServer;
